304bd1fb23d095602e3c08a4495ede0883c9bc48
Michael Kraft <michael.kraft@sparknet.net>
2016-11-01 13:34:07 -0500

    disabled magnification

--- bootstrap.php
+++ bootstrap.php
@@ -30,10 +30,10 @@
 	Preference::assert('Skip Disc Image Verify',          'com.apple.frameworks.diskimages skip-verify',             true,              false);
 	Preference::assert('Skip Disk Image Verify (Locked)', 'com.apple.frameworks.diskimages skip-verify-locked',      true,              false);
 	Preference::assert('Skip Disk Image Verify (Remote)', 'com.apple.frameworks.diskimages skip-verify-remote',      true,              false);
-	Preference::assert('Auto-Hide Dock',                  'com.apple.dock autohide',                                 true,             false);
-	Preference::assert('Dock Tilesize',                   'com.apple.dock tilesize',                                 58,                false);
-	Preference::assert('Dock Magnification',              'com.apple.dock magnification',                            true,              false);
-	Preference::assert('Dock Magnified Size',             'com.apple.dock largesize',                                80,                false);
+	Preference::assert('Auto-Hide Dock',                  'com.apple.dock autohide',                                 false,             false);
+	Preference::assert('Dock Tilesize',                   'com.apple.dock tilesize',                                 64,                false);
+	Preference::assert('Dock Magnification',              'com.apple.dock magnification',                            false,             false);
+	Preference::assert('Dock Magnified Size',             'com.apple.dock largesize',                                64,                false);
 	Preference::assert('Auto-Arrange Spaces',             'com.apple.dock mru-spaces',                               false,             false);
 	Preference::assert('Screensaver Password Delay',      'com.apple.screensaver askForPasswordDelay',               5,                 false);
 	Preference::assert('Default Terminal Window',         'com.Apple.Terminal "Default Window Settings"',            'Pro',             false);
